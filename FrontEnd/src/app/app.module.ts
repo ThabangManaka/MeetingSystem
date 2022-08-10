a06e6e2896c8416808b87d4c03a9c522e8de77b1
ThabangManaka <thabangdansteyn@gmail.com>
2022-08-10 23:01:59 +0200

    Adding Components

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeetingListComponent } from './meeting-list/meeting-list.component';
+import { NavbvarComponent } from './navbvar/navbvar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeetingListComponent,
+    NavbvarComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
---
+++ FrontEnd/src/app/navbvar/navbvar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbvar',
+  templateUrl: './navbvar.component.html',
+  styleUrls: ['./navbvar.component.scss']
+})
+export class NavbvarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
